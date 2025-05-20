97fead7a18bcb396b655a6c30ba7143abdd758de
Vojtěch Moravec <61109712+v-moravec@users.noreply.github.com>
2025-05-20 17:45:59 +0200

    [DASH-203] Add user preferences. (#79)

--- client/src/protoFleet/features/settings/components/General.tsx
+++ client/src/protoFleet/features/settings/components/General.tsx
@@ -1,9 +1,57 @@
+import { useState } from "react";
+import Row from "@/shared/components/Row";
+import {
+  TemperatureUnitsSwitcher,
+  ThemeSwitcher,
+  usePreferences,
+} from "@/shared/features/preferences";
+import { convertToSentenceCase } from "@/shared/utils/stringUtils";
+
 const General = () => {
+  const [showThemeSwitcher, setShowThemeSwitcher] = useState(false);
+  const [showTemperatureUnitsSwitcher, setShowTemperatureUnitsSwitcher] =
+    useState(false);
+  const { theme, temperatureUnits } = usePreferences();
+
   return (
     <>
-      <h2 className="font-size text-heading-200 text-text-primary">
-        Settings - General
-      </h2>
+      <div className="mx-auto max-w-xl">
+        <h3 className="mb-2 text-heading-100">Preferences</h3>
+        <Row className="flex justify-between">
+          <h4 className="text-emphasis-300">Theme</h4>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              setShowThemeSwitcher(true);
+            }}
+            className="text-300 text-intent-warning-fill hover:underline"
+          >
+            {convertToSentenceCase(theme)}
+          </a>
+          {showThemeSwitcher && (
+            <ThemeSwitcher onClickDone={() => setShowThemeSwitcher(false)} />
+          )}
+        </Row>
+        <Row className="flex justify-between">
+          <h4 className="text-emphasis-300">Temperature</h4>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              setShowTemperatureUnitsSwitcher(true);
+            }}
+            className="text-300 text-intent-warning-fill hover:underline"
+          >
+            {convertToSentenceCase(temperatureUnits)}
+          </a>
+          {showTemperatureUnitsSwitcher && (
+            <TemperatureUnitsSwitcher
+              onClickDone={() => setShowTemperatureUnitsSwitcher(false)}
+            />
+          )}
+        </Row>
+      </div>
     </>
   );
 };
