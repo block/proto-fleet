d87db21edc4528e8da30b63983e9f799617188a4
Marvin Charles <75264845+mcharles-square@users.noreply.github.com>
2025-05-22 20:12:37 +0300

    Migrate from nginx to httpd for streaming support (#83)

--- deploy/develop/frontend/dashboard/docker-entrypoint.sh
+++ deploy/develop/frontend/dashboard/docker-entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -e
 
+# Check if the API_PROXY_URL environment variable is set
+if [ -z "$API_PROXY_URL" ]; then
+    echo "Error: API_PROXY_URL environment variable is not set. Exiting."
+    exit 1
+fi
+
 # Process templates
-envsubst '${API_PROXY_URL}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${API_PROXY_URL}' < /usr/local/apache2/conf/templates/httpd.conf.template > /usr/local/apache2/conf/httpd.conf
+
+# Make sure Apache can read the SSL certificates
+chmod 644 /usr/local/apache2/ssl/server.crt
+chmod 600 /usr/local/apache2/ssl/server.key
 
 # Execute the CMD
 exec "$@"
