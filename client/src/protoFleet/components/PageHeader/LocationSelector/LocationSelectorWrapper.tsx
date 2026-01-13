6f5c6cfa3cba3485f0f36bb3ca0b1d5c6ecd2b47
Marvin Charles <75264845+mcharles-square@users.noreply.github.com>
2026-01-13 23:34:46 +0300

    [chore] Add beta to location selector (#850)

--- client/src/protoFleet/components/PageHeader/LocationSelector/LocationSelectorWrapper.tsx
+++ client/src/protoFleet/components/PageHeader/LocationSelector/LocationSelectorWrapper.tsx
@@ -2,7 +2,7 @@ import LocationSelector from "./LocationSelector";
 
 const LocationSelectorWrapper = () => {
   // TODO load location from API
-  const location = "Proto Fleet";
+  const location = "Proto Fleet Beta";
   const loading = false;
 
   return <LocationSelector location={location} loading={loading} />;
