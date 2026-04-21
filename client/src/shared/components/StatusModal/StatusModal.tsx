d4c8e1cfc2c76d55eda9285b9db44aa1b3f5eed8
Matt Flesher <matt.flesher@gmail.com>
2026-04-21 15:26:35 -0700

    Use standard 640px width for miner status modal (#15)

--- client/src/shared/components/StatusModal/StatusModal.tsx
+++ client/src/shared/components/StatusModal/StatusModal.tsx
@@ -44,7 +44,6 @@ export function StatusModal<TComponentAddress = any>({
         onIconClick={showBack ? componentData.onNavigateBack : undefined}
         onDismiss={componentData.onDismiss}
         open={open}
-        size="large"
       >
         <ComponentStatusModalContent {...componentData.props} />
       </Modal>
@@ -54,7 +53,7 @@ export function StatusModal<TComponentAddress = any>({
   // Fall back to miner status view
   const minerData = getMinerStatus();
   return (
-    <Modal title={minerData.title} buttons={minerData.buttons} onDismiss={minerData.onDismiss} open={open} size="large">
+    <Modal title={minerData.title} buttons={minerData.buttons} onDismiss={minerData.onDismiss} open={open}>
       <MinerStatusModalContent {...minerData.props} />
     </Modal>
   );
