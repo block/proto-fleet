58a1ee3904479d53df781885c0361fff1584308c
flesher <mflesher@squareup.com>
2026-05-05 20:00:23 -0700

    chore(gen): refresh python sdk grpc stubs

--- server/sdk/v1/python/proto_fleet_sdk/generated/pb/driver_pb2_grpc.py
+++ server/sdk/v1/python/proto_fleet_sdk/generated/pb/driver_pb2_grpc.py
@@ -6,7 +6,7 @@ import warnings
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 from proto_fleet_sdk.generated.pb import driver_pb2 as pb_dot_driver__pb2
 
-GRPC_GENERATED_VERSION = '1.76.0'
+GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
