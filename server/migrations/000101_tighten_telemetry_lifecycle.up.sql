e9f1c1c1c987cfdab3f6627ec35df8bf84be9ed8
Ankit Goswami <ankitg@squareup.com>
2026-06-29 11:42:19 -0700

    Address telemetry lifecycle review feedback

--- server/migrations/000101_tighten_telemetry_lifecycle.up.sql
+++ server/migrations/000101_tighten_telemetry_lifecycle.up.sql
@@ -1,11 +1,11 @@
 SELECT remove_retention_policy('device_metrics', if_exists => true);
-SELECT add_retention_policy('device_metrics', INTERVAL '36 hours');
+SELECT add_retention_policy('device_metrics', INTERVAL '8 days');
 
 SELECT remove_compression_policy('device_metrics', if_exists => true);
 SELECT add_compression_policy('device_metrics', INTERVAL '6 hours');
 
 SELECT remove_retention_policy('miner_state_snapshots', if_exists => true);
-SELECT add_retention_policy('miner_state_snapshots', INTERVAL '7 days');
+SELECT add_retention_policy('miner_state_snapshots', INTERVAL '1 year');
 
 SELECT remove_compression_policy('miner_state_snapshots', if_exists => true);
 SELECT add_compression_policy('miner_state_snapshots', INTERVAL '6 hours');
