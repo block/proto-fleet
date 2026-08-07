8fc3607dedc47c32e7c43b432e38bb1408b80724
Ankit Goswami <ankitg@squareup.com>
2026-08-07 10:45:51 -0700

    feat(ha): install clean Debian hosts

--- deployment-files/ha/tests/test-profile.sh
+++ deployment-files/ha/tests/test-profile.sh
@@ -45,7 +45,7 @@ test_compose_uses_one_host_identity() {
 
     [[ "$(grep -c 'network_mode: host' "$rendered")" -eq 2 ]] ||
         fail "etcd and Patroni must both use host networking"
-    assert_not_contains "$rendered" "restart:"
+    assert_contains "$rendered" "restart: always"
     assert_not_contains "$rendered" "ports:"
     assert_not_contains "$rendered" "127.0.0.1"
     assert_contains "$rendered" "https://10.40.0.11:2379"
@@ -136,8 +136,17 @@ test_fleet_ha_contract() {
     assert_contains "${HA_DIR}/scripts/check-fleet-active.sh" "--noproxy '*'"
     assert_not_contains "${HA_DIR}/scripts/check-fleet-active.sh" "--insecure"
     assert_contains "${HA_DIR}/keepalived-systemd.conf.tmpl" "Restart=on-failure"
+    assert_contains "${HA_DIR}/keepalived-systemd.conf.tmpl" "After=proto-fleet-ha.service"
+    assert_contains "${HA_DIR}/keepalived-systemd.conf.tmpl" "PartOf=proto-fleet-ha.service"
     assert_contains "${HA_DIR}/keepalived-systemd.conf.tmpl" 'ExecStopPost=/usr/sbin/ip address flush to ${HA_VIRTUAL_IP}/32 dev ${HA_NETWORK_INTERFACE}'
     assert_contains "${HA_DIR}/firewall.nft.tmpl" "tcp dport 40000 drop"
+    assert_contains "${HA_DIR}/proto-fleet-ha-firewall.service" "ExecStart=/usr/sbin/nft -f /etc/proto-fleet/ha/firewall.nft"
+    assert_contains "${HA_DIR}/proto-fleet-ha-firewall.service" "Before=docker.service proto-fleet-ha.service"
+    assert_contains "${HA_DIR}/proto-fleet-ha.service" "Requires=proto-fleet-ha-firewall.service docker.service"
+    assert_contains "${HA_DIR}/proto-fleet-ha.service" "PartOf=docker.service"
+    assert_contains "${HA_DIR}/proto-fleet-ha.service" "ExecStart=/opt/proto-fleet/deployment/ha/fleet-ha start"
+    assert_contains "${HA_DIR}/proto-fleet-ha.service" "Restart=on-failure"
+    assert_contains "${HA_DIR}/docker-systemd.conf" "Requires=proto-fleet-ha-firewall.service"
 
     for nginx_config in "${HA_DIR}/../client/nginx.http.conf" "${HA_DIR}/../client/nginx.https.conf"; do
         assert_contains "$nginx_config" "location ^~ /api-proxy/health/ha"
