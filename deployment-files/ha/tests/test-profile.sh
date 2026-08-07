e6bb43062435107d6405d8b011b1b4dbbe5c4445
Ankit Goswami <ankitg@squareup.com>
2026-08-07 15:55:14 -0700

    Harden HA install failure recovery (#888)

--- deployment-files/ha/tests/test-profile.sh
+++ deployment-files/ha/tests/test-profile.sh
@@ -73,6 +73,7 @@ test_patroni_contract() {
     assert_not_contains "$bootstrap" 'PGDATABASE="$connection_url"'
     assert_contains "$dockerfile" 'ARG TIMESCALEDB_IMAGE_TAG=latest'
     assert_contains "$dockerfile" 'FROM proto-fleet-timescaledb:${TIMESCALEDB_IMAGE_TAG}'
+    assert_contains "$compose" "restart: on-failure"
 
     [[ "$(grep -E '^[[:space:]]*USER[[:space:]]+' "$dockerfile" | tail -n 1)" == "USER postgres" ]] ||
         fail "Patroni image must default to the postgres user"
