62cf7411e0d284cd2bb99935fe789633b63c3099
Ankit Goswami <ankitg@squareup.com>
2026-08-08 04:56:38 -0700

    Integrate updater handoff recovery with startup repair

--- server/internal/updater/manager_test.go
+++ server/internal/updater/manager_test.go
@@ -1954,22 +1954,6 @@ func TestRepairStartupRestoresLayoutWithoutStartingHAApplication(t *testing.T) {
 	assert.True(t, operation.RecoveryPending)
 }
 
-func TestRepairStartupRestoresInterruptedSelfUpdateBeforeState(t *testing.T) {
-	// Arrange
-	destination := installSelfUpdateForHandoffTest(t)
-	stateDir := filepath.Join(t.TempDir(), "state")
-
-	// Act
-	err := RepairStartup(Config{
-		InstallRoot: t.TempDir(), StateDir: stateDir, SelfUpdatePath: destination,
-	})
-
-	// Assert
-	require.ErrorIs(t, err, ErrInterruptedSelfUpdateRestored)
-	assert.Equal(t, "old updater", mustReadFile(t, destination))
-	assert.DirExists(t, stateDir)
-}
-
 func TestRepairStartupDoesNotTouchSelfUpdateWhileManagerRuns(t *testing.T) {
 	// Arrange
 	installRoot := t.TempDir()
