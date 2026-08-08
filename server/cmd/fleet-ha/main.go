8aeda9f4298a5a304fee944a219ae290603d5ace
Ankit Goswami <ankitg@squareup.com>
2026-08-07 22:58:48 -0700

    Preserve simplified HA stop contract

--- server/cmd/fleet-ha/main.go
+++ server/cmd/fleet-ha/main.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/google/uuid"
 
-	"github.com/block/proto-fleet/server/internal/ha"
 	"github.com/block/proto-fleet/server/internal/ha/deployment"
 	"github.com/block/proto-fleet/server/internal/updaterapi"
 )
